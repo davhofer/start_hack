f40df754160eca8451dfb19959410a9e7376daf6
Oli <ollehman@student.ethz.ch>
2022-03-25 04:58:05 +0100

    debugged preprocessing script

--- preprocessing.py
+++ preprocessing.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 import pickle
 import pandas as pd
@@ -5,17 +6,17 @@ import pandas as pd
 
 def station_name_to_coordinates(df):
     train_stations = pd.read_csv("analysis/train_stations.csv")
-    df = pd.merge(df, train_stations, left_on='start_loc', right_on='abk_bahnhof')
+    df = pd.merge(df, train_stations, left_on='start_loc', right_on='Name Haltestelle')
     df = df.drop(columns=['abk_bahnhof', "Name Haltestelle"]).rename(columns={"lat": "lat_from", "lon": "lon_from"})
-    df = pd.merge(df, train_stations, left_on='end_loc', right_on='abk_bahnhof')
-    df = df.drop(columns=["abk_bahnhof", "Name Haltestelle"]).rename(columns={"lat": "lat_to", "lon": "lon_to"})
+    df = pd.merge(df, train_stations, left_on='end_loc', right_on='Name Haltestelle')
+    df = df.drop(columns=["abk_bahnhof", "Name Haltestelle", "start_loc", "end_loc"]).rename(columns={"lat": "lat_to", "lon": "lon_to"})
     return df
 
 
 def estimate_capacity(df):
     model = pickle.load(open("model/capacity_model.pkl", "rb"))
-    df_X = df.drop(["lat_from", "lon_from", "lat_to", "lon_to", "travel_duration"], axis=1)
-    return model.predict(df_X)
+    df_X = df[["day_of_week", "week", "start_hour", "line_category"]]
+    return model.predict(df_X).astype(int)
     
 
 
@@ -33,5 +34,5 @@ def pre_process_data(df):
     df_data["travel_duration"] = (df_data["end"] - df_data["start"]).dt.total_seconds()//60
     df_data["capacity"] = estimate_capacity(df_data)
     df_data.drop(df_data[df_data["travel_duration"] < 0].index, inplace=True)
-    df_data_feat_eng = df_data.drop(["start", "end", "line", "latest_res_dt"], axis=1)
+    df_data_feat_eng = df_data.drop(["start", "end", "line"], axis=1)
     return df_data_feat_eng
